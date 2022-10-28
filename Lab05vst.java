4264d2700894d7fca265432cf08158cd4a3aa2a0
Joezishere <Joseph.zinni@vtsd.con>
2022-10-28 07:43:36 -0400

    Lab 5 100point version

--- Lab05vst.java
+++ Lab05vst.java
@@ -11,12 +11,49 @@ public class Lab05vst extends Applet
     public void paint(Graphics g)
     {
         int width = 980;
-        int height = 630;
+        int height = 620;
         int x1 = 10;
-        int y1 = 640;
+        int y1 = 630;
         int x2 = 990;
-        int y2 = 640;
+        int y2 = 630;
         g.drawRect(10,10,width,height);
+
+        for(int k = 0;  k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            x1+=28;
+            y2-=18;
+        }
+
+         x1 = 10;
+         y1 = 630;
+         x2 = 990;
+         y2 = 630;
+        for(int k = 0;  k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y1 -= 18;
+            x2 -= 28;
+        }
+
+        x1 = 990;
+        y1 = 10;
+        x2 = 10;
+        y2 = 10;
+        for(int k = 0;  k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y1 += 18;
+            x2 += 28;
+        }
+
+        x1 = 990;
+        y1 = 10;
+        x2 = 10;
+        y2 = 10;
+        for(int k = 0;  k < 35; k++) {
+            g.drawLine(x1, y1, x2, y2);
+            y2 += 18;
+            x1 -= 28;
+        }
+
     }
 }
 
--- Temp.java
+++ Temp.java
@@ -1,2 +1,21 @@
-public class Temp {
+import java.awt.*;
+import java.applet.*;
+
+public class Temp extends Applet {
+    public void paint(Graphics g){
+
+        int width = 20;
+        int height = 20;
+        int x = 50;
+        int y = 50;
+
+
+        for(int k = 0;  k < 50; k++){
+
+            g.drawOval(x,y,width,height );
+            x+=50;
+            y+=50;
+      }
+
+    }
 }
